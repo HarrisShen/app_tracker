a70b4c0169aad2aeb0abc3084b300be869db5053
RH Shen <srh9572@gmail.com>
2022-11-17 22:03:04 -0500

    fix version number, v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='trackerflask',
-    version='0.0.1',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
